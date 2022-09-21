6ad52f415b82b4ae1ec25fb3ceff05e952ba9b91
jhonmcc <jhonmcc@gmail.com>
2022-09-20 22:38:07 -0300

    finalizado uso do token e recuperacao dos dados, criado componente cabeçalho e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,10 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/rodape/rodape.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rodape',
+  templateUrl: './rodape.component.html',
+  styleUrls: ['./rodape.component.css']
+})
+export class RodapeComponent {
+
+  constructor() { }
+
+
+}
